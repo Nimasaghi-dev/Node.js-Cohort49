81361094718fd873d854af7b3ab64f7235d834ad
Nima saghikalhori <nimasaghi.dev@gmail.com>
2024-10-04 18:49:48 +0200

    1-basic-authentication

--- week3/practice-exercises/1-basic-authentication/script.js
+++ week3/practice-exercises/1-basic-authentication/script.js
@@ -8,8 +8,26 @@
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
-function printBooks() {
-  // YOUR CODE GOES IN HERE
-}
+const base64 = require('base-64');
+const encodedCredentials = 'YWRtaW46aHZnWDhLbFZFYQ==';
+const apiUrl = "https://restapiabasicauthe-sandbox.mxapps.io/api/books";
 
+async function printBooks() {
+  try {
+    const response = await fetch(apiUrl, {
+      headers: {
+        'Authorization': `Basic ${encodedCredentials}`
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 printBooks();
